421a976feef38b36106e6885de31becf22393b63
spacetk00 <paulorcmv@gmail.com>
2022-06-09 19:26:55 -0300

    Alterações adicionais no header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import {HeaderContainer, HeaderTitle} from './styled';
+import React from "react";
+import {
+  HeaderContainer,
+  HeaderCenterContainer,
+  HeaderTitle,
+  NavContainer,
+} from "./styled";
 
 export default function Header() {
- return ( 
-   <HeaderContainer>
-     <HeaderTitle>Taku's CityPop</HeaderTitle>
-     <hr></hr>
-     <button>Join</button>
-   </HeaderContainer>
- );
+  return (
+    <HeaderContainer>
+      <NavContainer>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Roulette</a>
+      </NavContainer>
+      <HeaderCenterContainer>
+        <HeaderTitle>TAKU'S CITYPOP</HeaderTitle>
+        <hr></hr>
+        <button>Join</button>
+      </HeaderCenterContainer>
+    </HeaderContainer>
+  );
 }
--- src/components/header/styled.js
+++ src/components/header/styled.js
@@ -1,18 +1,65 @@
 import styled from "styled-components";
+import background from "../../images/header/background.webp";
 
 const HeaderContainer = styled.header`
   width: 100vw;
   height: 40vh;
-  background-color: #171717;
+  background-image: url(${background});
+  background-position: center top;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const HeaderCenterContainer = styled.div`
+  width: 100%;
+  height: 85%;
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-content: center;
 `;
 
 const HeaderTitle = styled.h1`
   color: white;
   text-align: center;
+  font-family: "Inter", sans-serif;
+  font-size: 5em;
 `;
 
+const NavContainer = styled.nav`
+  height: 15%;
+  background-color: rgba(23, 23, 23, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-export {HeaderContainer, HeaderTitle}
+  a {
+    position: relative;
+    color: white;
+    text-decoration: none;
+    margin: 0 5%;
+  }
+
+  a:hover {
+    color: white;
+  }
+
+  a::before {
+    content: "";
+    position: absolute;
+    display: block;
+    width: 100%;
+    height: 1px;
+    bottom: 0;
+    left: 0;
+    background-color: white;
+    transform: scaleX(0);
+    transition: transform 0.3s ease;
+  }
+
+  a:hover::before {
+    transform: scaleX(1);
+  }
+`;
+
+export { HeaderContainer, HeaderCenterContainer, HeaderTitle, NavContainer };
